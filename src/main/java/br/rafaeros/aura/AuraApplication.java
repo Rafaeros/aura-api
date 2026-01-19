104e6beb3b68a7eef15878d4f0e67491fbc34836
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2026-01-19 01:26:59 -0300

    [feat] added jpa auditing

--- src/main/java/br/rafaeros/aura/AuraApplication.java
+++ src/main/java/br/rafaeros/aura/AuraApplication.java
@@ -2,8 +2,10 @@ package br.rafaeros.aura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AuraApplication {
 
     public static void main(String[] args) {
