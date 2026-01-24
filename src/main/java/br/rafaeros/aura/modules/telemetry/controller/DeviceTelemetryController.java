a9dd4c3d3629639cd1d64c06f3aff6dcd1f72568
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2026-01-24 18:01:45 -0300

    [feat] imporved save telemetry and get all telemetry by device id

--- src/main/java/br/rafaeros/aura/modules/telemetry/controller/DeviceTelemetryController.java
+++ src/main/java/br/rafaeros/aura/modules/telemetry/controller/DeviceTelemetryController.java
@@ -1,8 +1,13 @@
 package br.rafaeros.aura.modules.telemetry.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +19,7 @@ import br.rafaeros.aura.modules.telemetry.service.DeviceTelemetryService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/telemetry")
+@RequestMapping("/devices")
 public class DeviceTelemetryController {
 
     private final DeviceTelemetryService deviceTelemetryService;
@@ -23,11 +28,20 @@ public class DeviceTelemetryController {
         this.deviceTelemetryService = deviceTelemetryService;
     }
 
-    @PostMapping
+    @PostMapping("/telemetry")
     @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<DeviceTelemetryResponseDTO> create(@Valid @RequestBody DeviceTelemetryRequestDTO request) {
-        DeviceTelemetryResponseDTO saved = deviceTelemetryService.create(request);
+    public ResponseEntity<DeviceTelemetryResponseDTO> ingestTelemetry(
+            @Valid @RequestBody DeviceTelemetryRequestDTO request) {
+        DeviceTelemetryResponseDTO saved = deviceTelemetryService.ingest(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
 
+    @GetMapping("/{deviceId}/telemetry")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Page<DeviceTelemetryResponseDTO>> getHistory(
+            @PathVariable Long deviceId,
+            @PageableDefault(page = 0, size = 20, sort = "createdAt", direction = org.springframework.data.domain.Sort.Direction.DESC) Pageable pageable) {
+
+        return ResponseEntity.ok(deviceTelemetryService.listHistory(deviceId, pageable));
+    }
 }
